b2a8379fda9ff919cbaab48b7f0c1d945a8e9d03
ebeqiri2002 <eb52738@ubt-uni.net>
2022-06-19 21:27:11 +0200

    Create create table dbo.Department(UserMode).sql

---
+++ UserCRUD/create table dbo.Department(UserMode).sql
@@ -0,0 +1,59 @@
+create table dbo.Department(
+DepartmentId int identity(1,1),
+DepartmentName varchar(500)
+)
+select * from dbo.Department
+
+insert into dbo.Department Values('Support')
+
+create table dbo.Employee(
+EmployeeId int identity(1,1),
+EmployeeName varchar(500),
+Department varchar(500),
+DateOfJoining date,
+PhotoFileName varchar(500)
+)
+
+select * from dbo.Employee
+insert into dbo.Employee values('Edison','IT','2022-05-21','img.jpg')
+
+create table dbo.Nurse(
+NurseId int identity(1,1),
+NurseName varchar(500),
+Department varchar(500),
+DateOfJoining date,
+PhotoFileName varchar(500),
+NrDhomes int
+)
+
+select * from dbo.Nurse
+
+insert into dbo.Nurse values('Elmedin','IT','2022-05-13','image.png',3)
+
+create table dbo.Admin(
+AdminId int identity(1,1),
+AdminName varchar(500),
+Birthday date,
+Vendbanimi varchar(500),
+PhotoFileName varchar(500),
+Email varchar(500),
+AdminPassword varchar(500),
+AdminRole varchar(500)
+)
+
+select * from dbo.Admin
+
+insert into dbo.Admin values('Edison','2002-10-14','Shtime','image.png','elmedin@gmail.com','12345678','Admin')
+
+
+create table dbo.Dhoma(
+DhomaId int identity(1,1),
+DhomaName varchar(500),
+Department varchar(500),
+EmployeeName varchar(500),
+NurseName varchar(500),
+Slots varchar(500)
+)
+
+select * from dbo.Dhoma
+insert into dbo.Dhoma values('Edison','IT','doctor','nurse name','3/20')
